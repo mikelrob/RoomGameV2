acc06b7bfb5cbfd1f9b08b7bec263ec5b8f9934b
Michael Robinson <mikelrob@Michaels-MacBook-Air.local>
2013-11-03 16:25:25 +0000

    Initial Commit

---
+++ RoomGameV2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RoomGameV2
+//
+//  Created by Michael Robinson on 03/11/2013.
+//  Copyright (c) 2013 Michael Robinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
